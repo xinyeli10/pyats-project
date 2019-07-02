c71794318c5e2eb81b647085c19908224b4529f5
Sarah Li <xinyeli@cisco.com>
2019-07-02 13:38:41 -0400

    made some minor changes

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,5 +9,5 @@ for i in range(number_of_test_cases):
 	uid = int(input("testcase_%s_id: " % (i + 1)))
 	description = input("testcase_%s_description [OPTIONAL]: " % (i + 1))
 	src = "/{{ cookiecutter.project_name }}/testcases/template_testcases.py"
-	dest = "/{{ cookiecutter.project_name }}/testcases/testcase_" + (i + 1) + ".py"
+	dest = "/{{ cookiecutter.project_name }}/testcases/testcase_" + str(i + 1) + ".py"
 	shutil.copy2(src, dest)
