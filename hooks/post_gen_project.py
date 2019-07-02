d4ebdc69a7a83d63b12a309b5052a42da6823455
Sarah Li <xinyeli@cisco.com>
2019-07-02 14:17:03 -0400

    made some minor changes

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,14 +3,16 @@ import shutil
 print("------------------------------")
 print("POST GEN HOOK\n\n")
 
+# create a certain number of testcases, as specified by the user
 number_of_test_cases = int(input("number_of_test_cases: "))
 for i in range(number_of_test_cases):
-	name = input("testcase_%s_name [OPTIONAL]: " % (i + 1))
-	uid = int(input("testcase_%s_id: " % (i + 1)))
-	description = input("testcase_%s_description [OPTIONAL]: " % (i + 1))
+    name = input("testcase_%s_name [OPTIONAL]: " % (i + 1))
+    uid = int(input("testcase_%s_id: " % (i + 1)))
+    description = input("testcase_%s_description [OPTIONAL]: " % (i + 1))
 
-	src = "../{{ cookiecutter.project_name }}/testcases/template_testcases.py"
-	dest = "../{{ cookiecutter.project_name }}/testcases/testcase_" + str(i + 1) + ".py"
-	if name: # file name not specified
-		dest = "../{{ cookiecutter.project_name }}/testcases/" + name + ".py"
-	shutil.copyfile(src, dest)
+    src = "../{{ cookiecutter.project_name }}/testcases/template_testcases.py"
+    dest = "../{{ cookiecutter.project_name }}/testcases/testcase_" + str(i + 1) + ".py"
+    if name: # file name not specified
+        dest = "../{{ cookiecutter.project_name }}/testcases/" + name + ".py"
+    shutil.copy2(src, dest)
+
