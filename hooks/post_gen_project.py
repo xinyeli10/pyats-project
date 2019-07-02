73af1a9832898502df91aafacf819638f2331cc5
Sarah Li <xinyeli@cisco.com>
2019-07-02 14:01:59 -0400

    added ability to generate multiple testcases

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,6 +8,9 @@ for i in range(number_of_test_cases):
 	name = input("testcase_%s_name [OPTIONAL]: " % (i + 1))
 	uid = int(input("testcase_%s_id: " % (i + 1)))
 	description = input("testcase_%s_description [OPTIONAL]: " % (i + 1))
+
 	src = "../{{ cookiecutter.project_name }}/testcases/template_testcases.py"
 	dest = "../{{ cookiecutter.project_name }}/testcases/testcase_" + str(i + 1) + ".py"
+	if name: # file name not specified
+		dest = "../{{ cookiecutter.project_name }}/testcases/testcase_" + name + ".py"
 	shutil.copyfile(src, dest)
