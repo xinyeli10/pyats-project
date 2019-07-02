e606062c31b2ad7364a2ce428945c0a539a99f17
Sarah Li <xinyeli@cisco.com>
2019-07-02 12:09:59 -0400

    made some minor changes

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,1 +1,5 @@
-print("post gen hook")
+print("------------------------------")
+print("POST GEN HOOK\n\n")
+
+number_of_test_cases = input("number_of_test_cases: ")
+print("You entered %d test cases" % number_of_test_cases)
