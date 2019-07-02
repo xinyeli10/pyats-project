482b262b76bb7c23ff408fe5855ebc12c3ab7a99
Sarah Li <xinyeli@cisco.com>
2019-07-02 14:42:07 -0400

    changed some variable names

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,13 +6,13 @@ print("POST GEN HOOK\n\n")
 # create a certain number of testcases, as specified by the user
 number_of_test_cases = int(input("number_of_test_cases: "))
 for i in range(number_of_test_cases):
-    name = input("testcase_%s_name [OPTIONAL]: " % (i + 1))
+    filename = input("testcase_%s_name [OPTIONAL]: " % (i + 1))
     uid = int(input("testcase_%s_id: " % (i + 1)))
     description = input("testcase_%s_description [OPTIONAL]: " % (i + 1))
 
-    src = "../{{ cookiecutter.project_name }}/testcases/template_testcases.py"
-    dest = "../{{ cookiecutter.project_name }}/testcases/testcase_" + str(i + 1) + ".py"
-    if name: # file name not specified
-        dest = "../{{ cookiecutter.project_name }}/testcases/" + name + ".py"
-    shutil.copy2(src, dest)
+    src_file = "../{{ cookiecutter.project_name }}/testcases/template_testcases.py"
+    dest_file = "../{{ cookiecutter.project_name }}/testcases/testcase_" + str(i + 1) + ".py"
+    if filename: # file name not specified
+        dest_file = "../{{ cookiecutter.project_name }}/testcases/" + filename + ".py"
+    shutil.copy2(src_file, dest_file)
 
