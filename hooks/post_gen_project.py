05dc9786c2308363c8a7bc91733fff969a91954e
Sarah Li <xinyeli@cisco.com>
2019-07-02 13:26:34 -0400

    made some minor changes

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,11 @@
 print("------------------------------")
 print("POST GEN HOOK\n\n")
 
-number_of_test_cases = input("number_of_test_cases: ")
+number_of_test_cases = int(input("number_of_test_cases: "))
 for i in range(number_of_test_cases):
 	name = input("testcase_%s_name [OPTIONAL]: " % (i + 1))
 	print(name)
-	uid = input("testcase_%s_id: " % (i + 1))
+	uid = int(input("testcase_%s_id: " % (i + 1)))
 	print(uid)
 	description = input("testcase_%s_description: " % (i + 1))
 	print(description)
