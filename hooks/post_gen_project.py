c372be604b2bbd4b8e7e3f7453263e19fafc9d58
Sarah Li <xinyeli@cisco.com>
2019-07-02 15:25:58 -0400

    made some minor changes

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,8 @@
 import shutil
 import fileinput
 
-print("------------------------------")
-print("POST GEN HOOK\n\n")
+print("\n\n------------------------------")
+print("POST GEN HOOK\n")
 
 # create a certain number of testcases, as specified by the user
 number_of_test_cases = int(input("number_of_test_cases: "))
@@ -10,12 +10,13 @@ for i in range(number_of_test_cases):
     filename = input("testcase_%s_name [OPTIONAL]: " % (i + 1))
     uid = int(input("testcase_%s_id: " % (i + 1)))
     description = input("testcase_%s_description [OPTIONAL]: " % (i + 1))
+    print()
 
+    # making a copy of template_testcases.py
     src_file = "../{{ cookiecutter.project_name }}/testcases/template_testcases.py"
     dest_file = "../{{ cookiecutter.project_name }}/testcases/testcase_" + str(i + 1) + ".py"
     if filename: # file name not specified
         dest_file = "../{{ cookiecutter.project_name }}/testcases/" + filename + ".py"
-    # make a copy of template_testcases.py
     shutil.copy2(src_file, dest_file)
 
     # replacing text in the testcase file
