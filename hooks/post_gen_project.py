2f363b5bde97b9a8bc9f54739be2834bf46279ff
Sarah Li <xinyeli@cisco.com>
2019-07-02 13:36:07 -0400

    made some minor changes

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,13 @@
+import shutil
+
 print("------------------------------")
 print("POST GEN HOOK\n\n")
 
 number_of_test_cases = int(input("number_of_test_cases: "))
 for i in range(number_of_test_cases):
 	name = input("testcase_%s_name [OPTIONAL]: " % (i + 1))
-	print(name)
 	uid = int(input("testcase_%s_id: " % (i + 1)))
-	print(uid)
-	description = input("testcase_%s_description: " % (i + 1))
-	print(description)
+	description = input("testcase_%s_description [OPTIONAL]: " % (i + 1))
+	src = "/{{ cookiecutter.project_name }}/testcases/template_testcases.py"
+	dest = "/{{ cookiecutter.project_name }}/testcases/testcase_" + (i + 1) + ".py"
+	shutil.copy2(src, dest)
