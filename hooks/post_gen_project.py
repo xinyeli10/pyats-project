9379b6796fa3b7c861375051777f2852b070fdd5
Sarah Li <xinyeli@cisco.com>
2019-07-04 13:59:02 -0400

    made some minor changes

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,21 +1,20 @@
+import os
 import logging
 import shutil
 import fileinput
 
 logger = logging.getLogger(__name__)
 
-def main():
-    logger.info("\n\n")
-
+def generate_testcases():
     # create a certain number of testcases, as specified by the user
-    number_of_test_cases = int(input("number_of_test_cases: "))
-    for i in range(number_of_test_cases):
+    number_of_testcases = int(input("number_of_testcases: "))
+    for i in range(number_of_testcases):
         filename = input("testcase_%s_name [OPTIONAL]: " % (i + 1))
         if not filename:
             filename = "testcase_" + str(i + 1)
         uid = int(input("testcase_%s_id: " % (i + 1)))
         description = input("testcase_%s_description [OPTIONAL]: " % (i + 1))
-        logger.info("\n")
+        print("\n")
 
         # making a copy of template_testcases.py
         src_file = "../{{ cookiecutter.project_name }}/testcases/template_testcases.py"
@@ -31,6 +30,27 @@ def main():
                 filedata = filedata.replace(src, target)
         with open(dest_file, 'w') as file:
             file.write(filedata)
+
+
+def generate_datafile():
+    src_file = "../{{ cookiecutter.project_name }}/data/template_datafile.py"
+    
+    include_datafile = input("include_datafile [yes]: ")
+    if 'y' in include_datafile: # yes, include the datafile
+        filename = input("datafile_name [template_datafile]: ")
+        if not filename:
+            filename = "template_datafile"
+        dest_file = "../{{ cookiecutter.project_name }}/data/" + filename + ".py"
+        os.rename(src_file, dest_file)
+    else: # no, don't include the datafile
+        os.remove(src_file)
+
+
+def main():
+    print("\n\n")
+    generate_testcases()
+    generate_datafile()
+    
 
 
 if __name__ == "__main__":
